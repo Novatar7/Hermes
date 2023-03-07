32591936e418dbeae54d9124f172e44f3d5400d8
Novatar7 <50675491+Novatar7@users.noreply.github.com>
2023-03-07 21:23:11 +0545

    migrations

--- customer/models.py
+++ customer/models.py
@@ -4,22 +4,22 @@ from django.db import models
 class MenuItem(models.Model):
     name= models.CharField(max_length=100)
     description=models.TextField()
-    image = models.ImageFeild(upload_to='menu_images/')
-    price = models.DecimalFeild(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     category =models.ManyToManyField('Category',related_name='item')
 
-    def __str__(self) -> str:
-        return super().__str__()
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
-    def __str__(self) -> str:
-        return super().__str__()
+    def __str__(self):
+        return self.name
     
 class OrderModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
-    price = models.DecimalFeild(max_digits=7, decimal_places=2)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
 
     def __str__(self) -> str:
