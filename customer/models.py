e828507cca16e3cccdf3c012950d67977aa77ff0
Novatar7 <50675491+Novatar7@users.noreply.github.com>
2023-03-07 20:45:49 +0545

    Update models.py

--- customer/models.py
+++ customer/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class MenuItem(models.Model):
     name= models.CharField(max_length=100)
     description=models.TextField()
-    image = models.ImageFeild(upload_to='menu_image/')
-    price = models.DecimalFeild(max_digits=5, decimal_place=2)
+    image = models.ImageFeild(upload_to='menu_images/')
+    price = models.DecimalFeild(max_digits=5, decimal_places=2)
     category =models.ManyToManyField('Category',related_name='item')
 
     def __str__(self) -> str:
@@ -20,7 +20,7 @@ class Category(models.Model):
 class OrderModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     price = models.DecimalFeild(max_digits=7, decimal_places=2)
-    items = models.ManyToManyField('MenuItem', related_name='orer', blank=True)
+    items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
 
     def __str__(self) -> str:
         return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
