f4df1f617d9afbc27cce390ff310a694395db41c
Novatar7 <np03s180025@heraldcollege.edu.np>
2023-05-21 02:09:03 +0545

    Menu ad order confirmation maintained

--- Hermes/urls.py
+++ Hermes/urls.py
@@ -14,16 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from customer.views import Index,About,Order
+from customer.views import Index,About,Order,Menu,MenuSearch,OrderConfirmation
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',Index.as_view(),name='index'),
     path('about/',About.as_view(), name='about'),
     path('order',Order.as_view(), name='order'),
-        
+    path('menu',Menu.as_view(), name= 'menu'),
+    path('menu/search/',MenuSearch.as_view, name='menu-search'),
+    path('order-confirmation/<int:pk>', OrderConfirmation.as_view(),
+         name='order-confirmation')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
